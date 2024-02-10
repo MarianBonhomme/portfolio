40a9245e0333f86dd82bbf914361708a47554fde
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-02-10 20:12:55 +0100

    responsive 2

--- src/components/SliderComponent.jsx
+++ src/components/SliderComponent.jsx
@@ -48,18 +48,18 @@ export default function SliderComponent() {
       >
         {projects.map((project, index) => (
           <SwiperSlide key={index}>
-            <img src={`/assets/images/${project.img}`} className="absolute top-1/2 -translate-y-[90%] sm:-translate-y-[75%]"/>
-            <div className="slide-content absolute top-[50%] sm:top-[60%] 2xl:top-[63%] sm:-left-1/4 w-full sm:w-[150%] lg:-left-1/2 lg:w-[200%] flex flex-col items-center z-50">
+            <img src={`/assets/images/${project.img}`} className="absolute top-1/2 -translate-y-[100%] sm:-translate-y-[75%]"/>
+            <div className="slide-content absolute top-[45%] sm:top-[60%] 2xl:top-[63%] sm:-left-1/4 w-full sm:w-[150%] lg:-left-1/2 lg:w-[200%] flex flex-col items-center z-50">
               <TitleComponent text={project.title} />
               <p className="text-center text-xxs sm:text-sm 2xl:text-xl leading-loose my-2 xl:my-5">{project.description}</p>
               <ButtonComponent text="see more" clicked={() => displayProjectDetails(project)} />
             </div>
           </SwiperSlide>
         ))}
-        <div className={`arrow-prev fixed bottom-[10%] sm:bottom-[15%] left-1/4 z-40 text-5xl 2xl:text-7xl font-black transition-all duration-500 
+        <div className={`arrow-prev fixed bottom-[15%] left-1/4 z-40 text-5xl 2xl:text-7xl font-black transition-all duration-500 
           ${currentSection === 2 ? 'cursor-pointer' : 'opacity-0'}
           ${!prevButtonVisible && 'opacity-0 cursor-default'}`} >←</div>
-        <div className={`arrow-next fixed bottom-[10%] sm:bottom-[15%] right-1/4 z-40 text-5xl 2xl:text-7xl font-black transition-all duration-500 
+        <div className={`arrow-next fixed bottom-[15%] right-1/4 z-40 text-5xl 2xl:text-7xl font-black transition-all duration-500 
           ${currentSection === 2 ? 'cursor-pointer' : 'opacity-0'}
           ${!nextButtonVisible && 'opacity-0 cursor-default'}`} >→</div>
       </Swiper>      
