09d787223d8254bb22feefa0cdb81d9f63165224
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-02-24 09:59:47 +0100

    fix responsive slider

--- src/components/SliderComponent.jsx
+++ src/components/SliderComponent.jsx
@@ -48,8 +48,8 @@ export default function SliderComponent() {
       >
         {projects.map((project, index) => (
           <SwiperSlide key={index}>
-            <img src={`/assets/images/${project.img}`} className="absolute top-1/2 -translate-y-[100%] sm:-translate-y-[75%]" alt={project.title}/>
-            <div className="slide-content absolute top-[50%] sm:top-[60%] 2xl:top-[63%] sm:-left-1/4 w-full sm:w-[150%] lg:-left-1/2 lg:w-[200%] flex flex-col items-center z-50">
+            <img src={`/assets/images/${project.img}`} className="absolute top-1/2 -translate-y-[120%] sm:-translate-y-[75%]" alt={project.title}/>
+            <div className="slide-content absolute top-[45%] sm:top-[60%] 2xl:top-[63%] sm:-left-1/4 w-full sm:w-[150%] lg:-left-1/2 lg:w-[200%] flex flex-col items-center z-50">
               <TitleComponent text={project.title} />
               <p className="text-center text-sm 2xl:text-xl leading-loose my-2 xl:my-5">{project.description}</p>
               <ButtonComponent text="see more" clicked={() => displayProjectDetails(project)} />
